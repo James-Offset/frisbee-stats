308f7648ad2fb0d053510083464dab61709d9ecd
James Butcher <jab319@cantab.ac.uk>
2024-11-05 16:50:01 +0000

    User Interface Started
    Got the GUI up and running, with tabs for roster and games

--- central_doc.py
+++ central_doc.py
@@ -20,27 +20,134 @@ And that's it I think.
 """
 
 """Third Party Code"""
-
+import tkinter as tk
+from tkinter import ttk
 
 """My Code"""
 from data_extractor import DataExtractor
+from frisbee_match import FrisbeeGame
+from team_roster import Team
 
 
 
-
-class HomePage():
+class MainGUI():
     """This class is the home page for the app and calls all the subclasses and functions.
     The core initial function is to collect tournament metadata"""
 
     def __init__(self):
-        """Initialise the programme. For now this means to import the data and start the first game"""
+        """Set up the GUI and allow the user to call subsequent functions"""
+
+
+        # create GUI window
+        self.root = tk.Tk()
+
+        # set root settings
+        self.root.geometry("600x600")
+        self.root.title("Ultimate Statistics User Interface")
+
+        # build notebook
+        self.notebook = ttk.Notebook(self.root)
+        self.notebook.pack(expand=True, fill='both')
+        self.game_tabs = {}
+
+        # set up key tabs
+        self._build_homepage()
+
+        # Create supporting classes
+        self.team = Team(self)
+
+        # create a blank dictionary to store our game classes
+        self.games = {}
+                
+
+
+
+
+
+        self.root.mainloop()
+        print("check")
+
+    def _build_homepage(self):
+        """Creates all the widgets and layouts for the homepage"""
+
+        # create the page and add it to the notebook
+        self.home_page = tk.Frame(self.notebook)
+        self.home_page.pack()
+        self.notebook.add(self.home_page, text="Home Page")
+
+        # add a label at the top
+        self.label = tk.Label(self.home_page, text="Frisbee Stats Homepage", font=('Arial', 18))
+        self.label.pack(padx=20, pady= 20)
+
+        # add a checkbox for custom data, and a button for data collection
+        self.checkstate = tk.IntVar()
+        self.check = tk.Checkbutton(self.home_page, text="Input Own Data", font=('Arial', 14), variable=self.checkstate)
+        self.check.pack(padx=10,pady=10)
+        self.data_button = tk.Button(self.home_page, text="Import Data", font=('Arial', 16), command=self.extract_stock_data)
+        self.data_button.pack(padx=10,pady=10)
+
+        # add a status label
+        self.status_label = tk.Label(self.home_page, text="Please click for data input")
+        self.status_label.pack()
+    
+
+
+
+    def extract_stock_data(self):
+        """Looks at the provided excel file and creates dictionaries of all player and game data"""
 
         # start by importing all the data
         extraction_tool = DataExtractor()
-        self.tournament_metadata, self.roster, self.raw_game_data = extraction_tool.import_stock_data()
+        self.tournament_metadata, self.imported_roster, self.raw_game_data = extraction_tool.import_stock_data()
+
+        resultsContents = tk.StringVar()
+        self.status_label['textvariable'] = resultsContents
+        resultsContents.set('New value to display')
+
+        # create player classes
+        for player in self.imported_roster:
+            self.team.new_player_entry(player, self.imported_roster[player]['Player Number'])
+
+        self._create_game_classes()
+                    
+
+
+    def _create_game_classes(self):
+        """Creates a class for each game played and runs the relevant functions"""
+
+
+        # create a loop for each game
+        for game_number in range(self.tournament_metadata['Number of Games']):
+            # Assign a name to the game
+            game_class_name = "Game " + str(game_number+1)
+
+            # create a dictionary to store performance data for each player
+            for player in self.imported_roster:
+                self.imported_roster[player][game_class_name] = {
+                    "Points Played" : 0,
+                    "Points Scored" : 0,
+                    "Points Won" : 0,
+                    "Turns Won" : 0,
+                    "Turns Lost" : 0
+                }
+
+            # find the set of raw data that applies to that game
+            for game_data in self.raw_game_data:
+                if game_class_name in game_data:
+                    break
+
+            # create a new class with the assembled input data
+            #!! self.games[game_class_name] = FrisbeeGame(self.imported_roster, self.raw_game_data[game_data])
+            self.games[game_class_name] = FrisbeeGame(self, game_class_name)
+
+            self.games[game_class_name].crunch_data_from_import(self.raw_game_data[game_data]['Turns per Point'])
+
+
+
+
 
 # call the main code
 if __name__ == "__main__":
-    the_tournament = HomePage()
+    tournament_gui = MainGUI()
 
     print("Check results")
--- frisbee_match.py
+++ frisbee_match.py
@@ -50,6 +50,153 @@ No. O / DG
 
 """
 
-class FrisbeeMatch():
-    def __init__(self):
+"""Third Party Code"""
+import tkinter as tk
+from tkinter import ttk
+
+
+class FrisbeeGame():
+    def __init__(self, parent, name):
         """This stores the core infomation"""
+
+        # copy out useful variables
+        self.parent = parent
+        self.name = name
+
+        # create a new tab in the GUI
+        self.game_page = tk.Frame(self.parent.notebook)
+        self.game_page.pack()
+        self.parent.notebook.add(self.game_page, text=self.name)
+
+        self._configure_table()
+
+    def _configure_table(self):
+        """Sets up the table that presents the game summary on the game tab"""
+
+        # configure 6 columns
+        self.pt_col = {}
+        self.game_page.columnconfigure(0, weight=1)
+        self.start_col = {}
+        self.game_page.columnconfigure(1, weight=2)
+        self.conced_col = {}
+        self.game_page.columnconfigure(2, weight=2)
+        self.win_col = {}
+        self.game_page.columnconfigure(3, weight=2)
+        self.result_col = {}
+        self.game_page.columnconfigure(4, weight=3)
+        self.score_col = {}
+        self.game_page.columnconfigure(5, weight=3)
+
+        # add heading labels
+        self.pt_num_head = tk.Label(self.game_page, text="Pt", font=('Arial', 18))
+        self.pt_num_head.grid(row=0 , column = 0, sticky=tk.W + tk.E)
+
+        self.start_head = tk.Label(self.game_page, text="Start", font=('Arial', 18))
+        self.start_head.grid(row=0 , column = 1, sticky=tk.W + tk.E)
+
+        self.concede_head = tk.Label(self.game_page, text="Turn Con", font=('Arial', 18))
+        self.concede_head.grid(row=0 , column = 2, sticky=tk.W + tk.E)
+
+        self.win_head = tk.Label(self.game_page, text="Turn Won", font=('Arial', 18))
+        self.win_head.grid(row=0 , column = 3, sticky=tk.W + tk.E)
+
+        self.result_head = tk.Label(self.game_page, text="Result", font=('Arial', 18))
+        self.result_head.grid(row=0 , column = 4, sticky=tk.W + tk.E)
+
+        self.score_head = tk.Label(self.game_page, text="Live Score", font=('Arial', 18))
+        self.score_head.grid(row=0 , column = 5, sticky=tk.W + tk.E)
+
+
+    def crunch_data_from_import(self, list_of_turns):
+        """When using imported data, this function calls other functions to calculate the results"""
+
+        self.work_out_team_performance()
+
+        self.list_of_turns = list_of_turns
+
+        # run the loop for each point to fill each category
+        for number_of_turns in self.list_of_turns:
+            self.evaluate_point(number_of_turns)
+
+    def work_out_team_performance(self):
+        """Looks at the number of turns and calcualtes the points won/lost and turnovers won/lost per point"""
+        self.team_performance = {
+            "Hold or Break" : [],
+            "Who Scored" : [],
+            "Team Score" : [],
+            "Op Score" : [],
+            "Disc Won" : [],
+            "Disc Lost" : []
+        }
+
+        #!! Fix this
+        self.defence_start = 'Us'
+
+        # record a modifier for who starts on defence
+        if self.defence_start == 'Us':
+            self.o_start_indicator = -1
+        else:
+            self.o_start_indicator = 1
+
+        self.point_number = 0
+        self.live_team_score = 0
+        self.live_opp_score = 0
+
+            
+
+    def evaluate_point(self, number_of_turns):
+        """Takes the information from a completed point and updates necessary variables"""
+
+        # increment point number
+        self.point_number += 1
+        
+        # work out whether the point was a hold or a break
+        hold_or_break = 1 - 2 * (number_of_turns % 2) # hold = 1, break = -1
+
+        # update our score accordingly
+        our_score = (self.o_start_indicator * hold_or_break + 1 ) / 2
+        self.team_performance["Team Score"].append(our_score)
+
+        # work out how many turnovers we won
+        turnovers_won = int((number_of_turns /2) + ((hold_or_break - 1) * self.o_start_indicator) / 4)
+        self.team_performance["Disc Won"].append(turnovers_won)
+        turnovers_conceded = int(number_of_turns - turnovers_won)
+        self.team_performance["Disc Lost"].append(turnovers_conceded)
+
+        # update the display table
+        self.pt_col[self.point_number] = tk.Label(self.game_page, text=self.point_number, font=('Arial', 16))
+        self.pt_col[self.point_number].grid(row=self.point_number, column=0, sticky=tk.W + tk.E)
+
+        if self.o_start_indicator == 1:
+            start_text = "Team"
+        else:
+            start_text = "Opp"
+        self.start_col[self.point_number] = tk.Label(self.game_page, text=start_text, font=('Arial', 16))
+        self.start_col[self.point_number].grid(row=self.point_number, column=1, sticky=tk.W + tk.E)
+
+        self.conced_col[self.point_number] = tk.Label(self.game_page, text=turnovers_conceded, font=('Arial', 16))
+        self.conced_col[self.point_number].grid(row=self.point_number, column=2, sticky=tk.W + tk.E)
+
+        self.win_col[self.point_number] = tk.Label(self.game_page, text=turnovers_won, font=('Arial', 16))
+        self.win_col[self.point_number].grid(row=self.point_number, column=3, sticky=tk.W + tk.E)
+
+        if our_score == 1:
+            result_text_1 = "Team "
+            self.live_team_score+=1
+        else:
+            result_text_1 = "Opp "
+            self.live_opp_score+=1
+        if hold_or_break == 1:
+            result_text_2 = "hold"
+        else:
+            result_text_2 = "break"
+        self.result_col[self.point_number] = tk.Label(self.game_page, text=result_text_1+result_text_2, font=('Arial', 16))
+        self.result_col[self.point_number].grid(row=self.point_number, column=4, sticky=tk.W + tk.E)
+
+        score_text = str(self.live_team_score) + " - " + str(self.live_opp_score)
+        self.score_col[self.point_number] = tk.Label(self.game_page, text=score_text, font=('Arial', 16))
+        self.score_col[self.point_number].grid(row=self.point_number, column=5, sticky=tk.W + tk.E)
+
+        # reset the modifier for who starts the next point on defense
+        self.o_start_indicator = 1 - (our_score * 2)
+            
